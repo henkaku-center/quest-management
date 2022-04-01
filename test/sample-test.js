19bea6fa888535423700dd5164566c1a71f3cc16
SamuraiT <t.yasukawa01@gmail.com>
2022-04-01 21:47:33 +0900

    refactor: Tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -5,30 +5,35 @@ const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
 
 describe('HenkakuQuest', function () {
-  it('cannot add quest without certain role', async () => {
-    const [owner, user1] = await ethers.getSigners()
+  let owner, user1
+  let nft, henkakuQuest
+  const quest = {
+    title: {
+      jp: '日本語1',
+      en: 'Foobar',
+    },
+    description: {
+      jp: 'fobar',
+      en: 'foobar',
+    },
+    category: 'foo',
+    limitation: 'per week 40',
+    amount: 100,
+    endedAt: 0,
+  }
+
+  beforeEach(async () => {
+    [owner, user1] = await ethers.getSigners()
     const Nft = await ethers.getContractFactory('NFT', owner)
-    const nft = await Nft.deploy()
+    nft = await Nft.deploy()
     await nft.deployed()
 
     const HenkakuQuest = await ethers.getContractFactory('HenkakuQuest')
-    const henkakuQuest = await HenkakuQuest.deploy(nft.address)
+    henkakuQuest = await HenkakuQuest.deploy(nft.address)
     await henkakuQuest.deployed()
+  })
 
-    const quest = {
-      title: {
-        jp: '日本語1',
-        en: 'Foobar'
-      },
-      description: {
-        jp: 'fobar',
-        en: 'foobar'
-      },
-      category: 'foo',
-      limitation: 'per week 40',
-      amount: 100,
-      endedAt: 0,
-    }
+  it('cannot add quest without certain role', async () => {
     await expect(
       henkakuQuest.connect(user1).save(quest)
     ).eventually.to.rejectedWith(Error)
@@ -36,34 +41,10 @@ describe('HenkakuQuest', function () {
     await henkakuQuest.addQuestCreationRole(user1.address)
     await henkakuQuest.connect(user1).save(quest)
     const data = await henkakuQuest.getQuests()
-    console.log(data)
     expect(data[0].title.jp).to.equal(quest.title.jp)
   })
 
   it('can update quest', async () => {
-    const [owner, user1] = await ethers.getSigners()
-    const Nft = await ethers.getContractFactory('NFT', owner)
-    const nft = await Nft.deploy()
-    await nft.deployed()
-
-    const HenkakuQuest = await ethers.getContractFactory('HenkakuQuest')
-    const henkakuQuest = await HenkakuQuest.deploy(nft.address)
-    await henkakuQuest.deployed()
-
-    const quest = {
-      title: {
-        jp: '日本語1',
-        en: 'Foobar'
-      },
-      description: {
-        jp: 'fobar',
-        en: 'foobar'
-      },
-      category: 'foo',
-      limitation: 'per week 40',
-      amount: 100,
-      endedAt: 0,
-    }
     await henkakuQuest.addQuestCreationRole(user1.address)
     await henkakuQuest.connect(user1).save(quest)
     let data = await henkakuQuest.getQuests()
@@ -74,30 +55,7 @@ describe('HenkakuQuest', function () {
     expect(data[0].title.jp).to.equal('ジャパニーズ')
   })
 
-  it('can close quest', async() => {
-    const [owner, user1] = await ethers.getSigners()
-    const Nft = await ethers.getContractFactory('NFT', owner)
-    const nft = await Nft.deploy()
-    await nft.deployed()
-
-    const HenkakuQuest = await ethers.getContractFactory('HenkakuQuest')
-    const henkakuQuest = await HenkakuQuest.deploy(nft.address)
-    await henkakuQuest.deployed()
-
-    const quest = {
-      title: {
-        jp: '日本語',
-        en: 'Foobar'
-      },
-      description: {
-        jp: 'fobar',
-        en: 'foobar'
-      },
-      category: 'foo',
-      limitation: 'per week 40',
-      amount: 100,
-      endedAt: 0,
-    }
+  it('can close quest', async () => {
     await henkakuQuest.addQuestCreationRole(user1.address)
     await henkakuQuest.save(quest)
     let data = await henkakuQuest.getQuests()
@@ -106,56 +64,13 @@ describe('HenkakuQuest', function () {
     await henkakuQuest.connect(user1).closeQuest(0)
     data = await henkakuQuest.getQuests()
     expect(data[0].endedAt).to.not.equal(0)
-
   })
 
   it('Add quests and returns them to only the henkaku member', async () => {
-    const [owner, user1] = await ethers.getSigners()
-    const Nft = await ethers.getContractFactory('NFT', owner)
-    const nft = await Nft.deploy()
-    await nft.deployed()
-
-    const HenkakuQuest = await ethers.getContractFactory('HenkakuQuest')
-    const henkakuQuest = await HenkakuQuest.deploy(nft.address)
-    await henkakuQuest.deployed()
-
-    console.log(nft.address, henkakuQuest.address)
-
     await nft.mint(true, owner.address)
-
-    const quests = [
-      {
-        title: {
-          jp: '日本語1',
-          en: 'Foobar'
-        },
-        description: {
-          jp: 'fobar',
-          en: 'foobar'
-        },
-        category: 'foo',
-        limitation: 'per week 40',
-        amount: 100,
-        endedAt: 0,
-      },
-      {
-        title: {
-          jp: '日本語2',
-          en: 'Foobar'
-        },
-        description: {
-          jp: 'fobar',
-          en: 'foobar'
-        },
-        category: 'foo',
-        limitation: 'per week 40',
-        amount: 100,
-        endedAt: 0,
-      },
-    ]
-    quests.forEach((quest) => henkakuQuest.save(quest))
+    await henkakuQuest.save(quest)
     const data = await henkakuQuest.getQuests()
-    expect(data[0].title.jp).to.equal(quests[0].title.jp)
+    expect(data[0].title.jp).to.equal(quest.title.jp)
 
     await expect(
       henkakuQuest.connect(user1).getQuests()
@@ -163,6 +78,6 @@ describe('HenkakuQuest', function () {
 
     await henkakuQuest.addMemberRole(user1.address)
     const data2 = await henkakuQuest.connect(user1).getQuests()
-    expect(data2[0].title.jp).to.equal(quests[0].title.jp)
+    expect(data2[0].title.jp).to.equal(quest.title.jp)
   })
 })
