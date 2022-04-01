eac0543c41f2a6afc2e498085c5ccb451aa46255
SamuraiT <t.yasukawa01@gmail.com>
2022-04-01 21:03:12 +0900

    refactor: Pretiier
    Signed-off-by: SamuraiT <t.yasukawa01@gmail.com>

--- contracts/HenkakuQuest.sol
+++ contracts/HenkakuQuest.sol
@@ -6,15 +6,20 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 interface IHenkakuMemberShip {
     function balanceOf(address _owner) external view returns (uint256);
+
     function tokenURI(uint256 _tokenId) external view returns (string memory);
-    function isCommunityMember(uint256 _tokenId) external view returns(bool);
+
+    function isCommunityMember(uint256 _tokenId) external view returns (bool);
+
     function ownerOf(uint256 tokenId) external view returns (address);
 }
 
 contract HenkakuQuest is AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    bytes32 public constant QUEST_CREATION_ROLE = keccak256("QUEST_CREATION_ROLE");
-    bytes32 public constant HENKAKU_MEMBER_ROLE = keccak256("HENKAKU_MEMBER_ROLE");
+    bytes32 public constant QUEST_CREATION_ROLE =
+        keccak256("QUEST_CREATION_ROLE");
+    bytes32 public constant HENKAKU_MEMBER_ROLE =
+        keccak256("HENKAKU_MEMBER_ROLE");
 
     struct Content {
         string title;
@@ -22,7 +27,7 @@ contract HenkakuQuest is AccessControl {
         string description;
         string limitation;
         uint256 amount;
-        uint endedAt;
+        uint256 endedAt;
     }
 
     struct Quest {
@@ -33,7 +38,7 @@ contract HenkakuQuest is AccessControl {
     address private memberShipNFTAddress;
     IHenkakuMemberShip private memberShipNFT;
     mapping(uint256 => Quest) quests;
-    uint private id;
+    uint256 private id;
 
     constructor(address _memberShipNftAddress) {
         _grantRole(ADMIN_ROLE, msg.sender);
@@ -41,27 +46,36 @@ contract HenkakuQuest is AccessControl {
     }
 
     function setNFTAddress(address _memberShipNftAddress) public {
-        require(hasRole(ADMIN_ROLE, msg.sender), 'You need to have an admin');
+        require(hasRole(ADMIN_ROLE, msg.sender), "You need to have an admin");
         memberShipNFTAddress = _memberShipNftAddress;
         memberShipNFT = IHenkakuMemberShip(memberShipNFTAddress);
     }
 
     function addAdminRole(address _to) public {
-        require(hasRole(ADMIN_ROLE, msg.sender),'You need to have an admin role to add Admin role');
+        require(
+            hasRole(ADMIN_ROLE, msg.sender),
+            "You need to have an admin role to add Admin role"
+        );
         _grantRole(ADMIN_ROLE, _to);
     }
 
     function addQuestCreationRole(address _to) public {
-        require(hasRole(ADMIN_ROLE, msg.sender),'You need to have an admin role to add quest creation role');
+        require(
+            hasRole(ADMIN_ROLE, msg.sender),
+            "You need to have an admin role to add quest creation role"
+        );
         _grantRole(QUEST_CREATION_ROLE, _to);
     }
 
     function addMemberRole(address _to) public {
-        require(hasRole(ADMIN_ROLE, msg.sender),'You need to have an admin role to add quest creation role');
+        require(
+            hasRole(ADMIN_ROLE, msg.sender),
+            "You need to have an admin role to add quest creation role"
+        );
         _grantRole(HENKAKU_MEMBER_ROLE, _to);
     }
 
-    function isMember(address _address) internal view returns(bool) {
+    function isMember(address _address) internal view returns (bool) {
         uint256 _balance = memberShipNFT.balanceOf(_address);
         if (_balance <= 0) {
             return false;
@@ -70,7 +84,7 @@ contract HenkakuQuest is AccessControl {
         uint256 _tokenId = 0;
         address _candidate;
         bool _isMember = false;
-        while(!_isMember) {
+        while (!_isMember) {
             _tokenId += 1;
             _candidate = memberShipNFT.ownerOf(_tokenId);
             _isMember = _candidate == _address;
@@ -81,19 +95,27 @@ contract HenkakuQuest is AccessControl {
     }
 
     function save(Quest memory _quest) public {
-        require(hasRole(QUEST_CREATION_ROLE, msg.sender) || hasRole(ADMIN_ROLE, msg.sender), 'You need to have Quest creation role or admin role to add a quest');
+        require(
+            hasRole(QUEST_CREATION_ROLE, msg.sender) ||
+                hasRole(ADMIN_ROLE, msg.sender),
+            "You need to have Quest creation role or admin role to add a quest"
+        );
         quests[id] = _quest;
         id += 1;
     }
 
-    function canReadQuest(address _address) public view returns(bool) {
-        return hasRole(QUEST_CREATION_ROLE, _address) || hasRole(ADMIN_ROLE, _address) || hasRole(HENKAKU_MEMBER_ROLE, _address) || isMember(_address);
+    function canReadQuest(address _address) public view returns (bool) {
+        return
+            hasRole(QUEST_CREATION_ROLE, _address) ||
+            hasRole(ADMIN_ROLE, _address) ||
+            hasRole(HENKAKU_MEMBER_ROLE, _address) ||
+            isMember(_address);
     }
 
-    function getQuests() public view returns(Quest[] memory){
-        require(canReadQuest(msg.sender), 'You dont have permission to obtain');
+    function getQuests() public view returns (Quest[] memory) {
+        require(canReadQuest(msg.sender), "You dont have permission to obtain");
         Quest[] memory _quests = new Quest[](id);
-        for (uint i = 0; i < id; i++) {
+        for (uint256 i = 0; i < id; i++) {
             _quests[i] = quests[i];
         }
 
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,39 +1,41 @@
-const chai = require("chai");
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
-const chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
+const chai = require('chai')
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
 
-describe("HenkakuQuest", function () {
-  it("cannot add quest without certain role", async() => {
-    const [owner, user1] = await ethers.getSigners();
-    const Nft = await ethers.getContractFactory("NFT", owner)
+describe('HenkakuQuest', function () {
+  it('cannot add quest without certain role', async () => {
+    const [owner, user1] = await ethers.getSigners()
+    const Nft = await ethers.getContractFactory('NFT', owner)
     const nft = await Nft.deploy()
     await nft.deployed()
 
-    const HenkakuQuest = await ethers.getContractFactory("HenkakuQuest");
-    const henkakuQuest = await HenkakuQuest.deploy(nft.address);
-    await henkakuQuest.deployed();
+    const HenkakuQuest = await ethers.getContractFactory('HenkakuQuest')
+    const henkakuQuest = await HenkakuQuest.deploy(nft.address)
+    await henkakuQuest.deployed()
 
     const quest = {
       jp: {
-        title: "hoge",
-        description: "fobar",
-        category: "foo",
+        title: 'hoge',
+        description: 'fobar',
+        category: 'foo',
         limitation: 'per week 40',
         amount: 100,
-        endedAt: 0
+        endedAt: 0,
       },
       en: {
-        title: "hoge",
-        description: "fobar",
-        category: "foo",
+        title: 'hoge',
+        description: 'fobar',
+        category: 'foo',
         limitation: 'per week 40',
         amount: 100,
-        endedAt: 0
-      }
+        endedAt: 0,
+      },
     }
-    await expect(henkakuQuest.connect(user1).save(quest)).eventually.to.rejectedWith(Error)
+    await expect(
+      henkakuQuest.connect(user1).save(quest)
+    ).eventually.to.rejectedWith(Error)
 
     await henkakuQuest.addQuestCreationRole(user1.address)
     await henkakuQuest.connect(user1).save(quest)
@@ -41,15 +43,15 @@ describe("HenkakuQuest", function () {
     expect(data[0].jp.title).to.equal(quest.en.title)
   })
 
-  it("Add quests and returns them to only the henkaku member", async () => {
-    const [owner, user1] = await ethers.getSigners();
-    const Nft = await ethers.getContractFactory("NFT", owner)
+  it('Add quests and returns them to only the henkaku member', async () => {
+    const [owner, user1] = await ethers.getSigners()
+    const Nft = await ethers.getContractFactory('NFT', owner)
     const nft = await Nft.deploy()
     await nft.deployed()
 
-    const HenkakuQuest = await ethers.getContractFactory("HenkakuQuest");
-    const henkakuQuest = await HenkakuQuest.deploy(nft.address);
-    await henkakuQuest.deployed();
+    const HenkakuQuest = await ethers.getContractFactory('HenkakuQuest')
+    const henkakuQuest = await HenkakuQuest.deploy(nft.address)
+    await henkakuQuest.deployed()
 
     console.log(nft.address, henkakuQuest.address)
 
@@ -58,49 +60,51 @@ describe("HenkakuQuest", function () {
     const quests = [
       {
         jp: {
-          title: "hoge",
-          description: "fobar",
-          category: "foo",
+          title: 'hoge',
+          description: 'fobar',
+          category: 'foo',
           limitation: 'per week 40',
           amount: 100,
-          endedAt: 0
+          endedAt: 0,
         },
         en: {
-          title: "hoge",
-          description: "fobar",
-          category: "foo",
+          title: 'hoge',
+          description: 'fobar',
+          category: 'foo',
           limitation: 'per week 40',
           amount: 100,
-          endedAt: 0
-        }
+          endedAt: 0,
+        },
       },
       {
         jp: {
-          title: "teehee",
-          description: "fobar",
-          category: "foo",
+          title: 'teehee',
+          description: 'fobar',
+          category: 'foo',
           limitation: 'per week 40',
           amount: 100,
-          endedAt: 0
+          endedAt: 0,
         },
         en: {
-          title: "teehee",
-          description: "fobar",
-          category: "foo",
+          title: 'teehee',
+          description: 'fobar',
+          category: 'foo',
           limitation: 'per week 40',
           amount: 100,
-          endedAt: 0
-        }
+          endedAt: 0,
+        },
       },
     ]
-    quests.forEach(quest => henkakuQuest.save(quest))
+    quests.forEach((quest) => henkakuQuest.save(quest))
     const data = await henkakuQuest.getQuests()
     expect(data[0].jp.title).to.equal(quests[0].jp.title)
 
-    await expect(henkakuQuest.connect(user1).getQuests()).eventually.to.rejectedWith(Error)
+    await expect(
+      henkakuQuest.connect(user1).getQuests()
+    ).eventually.to.rejectedWith(Error)
 
     await henkakuQuest.addMemberRole(user1.address)
     const data2 = await henkakuQuest.connect(user1).getQuests()
     expect(data2[0].jp.title).to.equal(quests[0].jp.title)
-  });
-});
+  })
+})
